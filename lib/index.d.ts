7eeccb59ee7026dc4d933ba41c8bed2a0ac73507
Mark Moss <mark.moss@tfc.com>
2022-01-03 10:59:08 -0500

    Updated default export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,11 @@
+import callApi from "./callApi";
 import execCommand from "./execCommand";
+import * as hash from './hash';
+import respondWithCode from "./responseWithCode";
 declare const _default: {
+    callApi: typeof callApi;
     execCommand: typeof execCommand;
+    hash: typeof hash;
+    respondWithCode: typeof respondWithCode;
 };
 export default _default;
